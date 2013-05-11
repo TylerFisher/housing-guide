e81ef8d069ec7f1ae58095c51fa5d431b7ae430d
kklai <kkrebeccalai@gmail.com>
2013-05-10 21:15:04 -0500

    Test

--- housing/housing/settings.py
+++ housing/housing/settings.py
@@ -19,7 +19,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'housing',
         'PASSWORD': 'wildk1tt3h!',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '/tmp/',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
