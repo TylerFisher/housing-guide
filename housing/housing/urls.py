4fe9c692f3f50d7b11ed3b2faef7f61043f943f6
Tyler Fisher <tyler.fisher@me.com>
2013-05-15 12:13:45 -0500

    oops

--- housing/housing/urls.py
+++ housing/housing/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic.simple import redirect_to
+from django.views.generic import RedirectView
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^housing/$', 'guide.views.home', name='home'),
-    url(r'^housing/$', redirect_to, {'url': '/housing/'}),
+    url(r'^housing/$', RedirectView.as_view('url': '/housing/')),
     url(r'^housing/hall/(?P<dorm_slug>[\w\-]+)/$', 'guide.views.detail', name='detail'),
     url(r'^housing/admin/', include(admin.site.urls)),
     url(r'^housing/json/', 'guide.views.get_shapes', name='get_shapes'),
