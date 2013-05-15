97c24c172865c77293b02fba466faadcf729bcc3
Tyler Fisher <tyler.fisher@me.com>
2013-05-15 13:20:48 -0400

    oh my god

--- housing/housing/urls.py
+++ housing/housing/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import RedirectView
+from django.views.generic.base import RedirectView
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
